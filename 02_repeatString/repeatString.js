5eeb1d47c99a8a450bb8b5826a11ee827881089b
maharg-wettham <5ttam.maharg@gmail.com>
2023-12-07 08:11:02 -0500

    Adds my solutions

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let newString = "";
+    if (num < 0) {
+        return "ERROR";
+    } else if (num === 0) {
+        return "";
+    } else {
+        for (let i = 1; i <= num; i++) {
+            newString = newString + string;
+        }
+        return newString;
+    }
 
 };
 
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let lowNum;
+    let highNum;
+    let sum = 0;
 
+    if (num1 < 0 || num2 < 0 || typeof num1 != "number" || typeof num2 != "number") {
+        return "ERROR";
+    }
+
+    if (num1 > num2) {
+        lowNum = num2;
+        highNum = num1;
+    } else {
+        lowNum = num1;
+        highNum = num2;
+    }
+
+
+    for (let i = lowNum; i <= highNum; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
